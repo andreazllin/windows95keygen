f5c0d7bc03ac64b293716066a211ee1820248258
Andrea <andrea.lin3110@gmail.com>
2021-01-11 06:55:28 +0100

    Made a real Next.js/React app

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,50 @@
-function Home() {
+import React, { useState } from 'react';
+import Head from 'next/head';
+import axios from 'axios';
+
+export default function Home() {
+  const [key, setKey] = useState('-')
+  const getKey = async (keyType) => {
+    setKey("Getting key...");
+    const url = `${window.location.href}api/${keyType}`; // API Call to get generated key.
+    try {
+      const response = await axios.get(url);
+      setKey(response.data.key);
+    } catch (error) {
+      setKey("There was an error...");
+    }
+  };
+
   return (
-    <div className="center">
-      <div className="container">
-        <div className="card">
-          <div className="card-title no-select">Generate a Windows NT 4.0 or Windows 95 Key!</div>
-          <div className="card-content">
-            <span className="no-select">Key: </span>
-            <span id="keyOutput"> - </span>
-          </div>
-          <div className="card-footer no-select">
-            <button className="btn" id="normalKeyButton">
-              {' '}
-              <span className="btn-text">Normal</span>{' '}
-            </button>
-            <button className="btn" id="oemKeyButton">
-              {' '}
-              <span className="btn-text">OEM</span>{' '}
-            </button>
-            <a target="_blank" href="https://github.com/nilaerdna/windows95keygen">
-              <button className="btn" id="githubButton">
-                {' '}
-                <span className="btn-text">GitHub</span>{' '}
+    <>
+      <Head>
+        <title>windows95keygen</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="center">
+        <div className="container">
+          <div className="card">
+            <div className="card-title no-select">Generate a Windows NT 4.0 or Windows 95 Key!</div>
+            <div className="card-content">
+              <span className="no-select">Key: </span>
+              <span id="keyOutput">{key}</span>
+            </div>
+            <div className="card-footer no-select">
+              <button className="btn" id="normalKeyButton">
+                <span className="btn-text" onClick={() => { getKey("normal") }}>Normal</span>
               </button>
-            </a>
+              <button className="btn" id="oemKeyButton">
+                <span className="btn-text" onClick={() => { getKey("oem") }}>OEM</span>
+              </button>
+              <a target="_blank" href="https://github.com/nilaerdna/windows95keygen">
+                <button className="btn" id="githubButton">
+                  <span className="btn-text">GitHub</span>
+                </button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <script src="script.js"></script>
-    </div>
-  );
+    </>
+  )
 }
-
-export default Home;
